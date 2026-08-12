08d04578b202ff6565578eba1668b95ce2c25a1b
AnthonyGaytan <anthonyluisgaytan@gmail.com>
2026-08-12 16:17:52 -0700

    Add project files.

---
+++ Module10Ch10DatabasesExercise/Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvStates = New System.Windows.Forms.DataGridView()
+        Me.btnPresidents = New System.Windows.Forms.Button()
+        Me.btnStates = New System.Windows.Forms.Button()
+        Me.btnPopulation = New System.Windows.Forms.Button()
+        Me.cboOptions = New System.Windows.Forms.ComboBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnCities = New System.Windows.Forms.Button()
+        CType(Me.dgvStates, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvStates
+        '
+        Me.dgvStates.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvStates.Location = New System.Drawing.Point(21, 150)
+        Me.dgvStates.Name = "dgvStates"
+        Me.dgvStates.RowHeadersWidth = 51
+        Me.dgvStates.RowTemplate.Height = 24
+        Me.dgvStates.Size = New System.Drawing.Size(669, 278)
+        Me.dgvStates.TabIndex = 0
+        '
+        'btnPresidents
+        '
+        Me.btnPresidents.Location = New System.Drawing.Point(21, 29)
+        Me.btnPresidents.Name = "btnPresidents"
+        Me.btnPresidents.Size = New System.Drawing.Size(172, 52)
+        Me.btnPresidents.TabIndex = 2
+        Me.btnPresidents.Text = "Get Presidents"
+        Me.btnPresidents.UseVisualStyleBackColor = True
+        '
+        'btnStates
+        '
+        Me.btnStates.Location = New System.Drawing.Point(382, 29)
+        Me.btnStates.Name = "btnStates"
+        Me.btnStates.Size = New System.Drawing.Size(173, 52)
+        Me.btnStates.TabIndex = 3
+        Me.btnStates.Text = "Get States"
+        Me.btnStates.UseVisualStyleBackColor = True
+        '
+        'btnPopulation
+        '
+        Me.btnPopulation.Location = New System.Drawing.Point(603, 29)
+        Me.btnPopulation.Name = "btnPopulation"
+        Me.btnPopulation.Size = New System.Drawing.Size(155, 52)
+        Me.btnPopulation.TabIndex = 4
+        Me.btnPopulation.Text = "Pop <  20 Million"
+        Me.btnPopulation.UseVisualStyleBackColor = True
+        '
+        'cboOptions
+        '
+        Me.cboOptions.FormattingEnabled = True
+        Me.cboOptions.Items.AddRange(New Object() {"Display State by Abbreviation", "Display only states where area < 2000"})
+        Me.cboOptions.Location = New System.Drawing.Point(727, 150)
+        Me.cboOptions.Name = "cboOptions"
+        Me.cboOptions.Size = New System.Drawing.Size(251, 24)
+        Me.cboOptions.TabIndex = 5
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(793, 222)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(100, 22)
+        Me.txtOutput.TabIndex = 6
+        '
+        'btnCities
+        '
+        Me.btnCities.Location = New System.Drawing.Point(226, 29)
+        Me.btnCities.Name = "btnCities"
+        Me.btnCities.Size = New System.Drawing.Size(137, 52)
+        Me.btnCities.TabIndex = 7
+        Me.btnCities.Text = "Get Cities"
+        Me.btnCities.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(990, 450)
+        Me.Controls.Add(Me.btnCities)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.cboOptions)
+        Me.Controls.Add(Me.btnPopulation)
+        Me.Controls.Add(Me.btnStates)
+        Me.Controls.Add(Me.btnPresidents)
+        Me.Controls.Add(Me.dgvStates)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvStates, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgvStates As DataGridView
+    Friend WithEvents btnPresidents As Button
+    Friend WithEvents btnStates As Button
+    Friend WithEvents btnPopulation As Button
+    Friend WithEvents cboOptions As ComboBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents btnCities As Button
+End Class
---
+++ Module10Ch10DatabasesExercise/Form1.vb
@@ -0,0 +1,113 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+
+    ' Connection to database (States.mdb must be in Debug folder)
+    Dim conn As New OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & Application.StartupPath & "\States.mdb")
+
+    ' Open connection when form loads
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conn.Open()
+    End Sub
+
+    ' Show all states
+    Private Sub btnStates_Click(sender As Object, e As EventArgs) Handles btnStates.Click
+        ShowAll()
+
+
+    End Sub
+
+
+    Private Sub ShowAll()
+
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter("SELECT * FROM States", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+
+    End Sub
+
+
+
+
+
+    ' Show population less than 1 million
+    Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
+
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter("SELECT * FROM States WHERE Population < 1000000", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+
+    End Sub
+
+
+    Private Sub ShowByAbbreviation()
+
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter(
+            "SELECT * FROM States WHERE Abbreviation = '" & txtOutput.Text & "'", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+
+    End Sub
+
+    Private Sub cboOptions_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOptions.SelectedIndexChanged
+        If cboOptions.SelectedIndex = 0 Then
+            DisplayStateByAbbreviation()
+        ElseIf cboOptions.SelectedIndex = 1 Then
+            DisplayLandAreaLess2000()
+        End If
+    End Sub
+
+
+    Private Sub DisplayLandAreaLess2000()
+
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter(
+        "SELECT * FROM States WHERE [Land Area] < 2000", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+
+    End Sub
+
+
+
+
+
+    Private Sub DisplayStateByAbbreviation()
+
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter(
+        "SELECT * FROM States WHERE Abbreviation = '" & txtOutput.Text & "'", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+
+    End Sub
+
+
+
+    Private Sub ShowAreaLessThan2000()
+
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter(
+            "SELECT * FROM States WHERE [Land Area] < 2000", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+
+    End Sub
+
+    Private Sub btnCities_Click(sender As Object, e As EventArgs) Handles btnCities.Click
+        Dim table As New DataTable
+        Dim adapter As New OleDbDataAdapter("SELECT * FROM Cities", conn)
+
+        adapter.Fill(table)
+        dgvStates.DataSource = table
+    End Sub
+End Class
